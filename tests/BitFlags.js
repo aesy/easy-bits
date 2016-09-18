9444cba05192ef01dd3be6b61054256dd5ed946b
Isak W <isak.wertwein@gmail.com>
2016-09-18 04:28:55 +0200

    tests for BitFlags & Enums

---
+++ tests/BitFlags.js
@@ -0,0 +1,21 @@
+import { expect } from 'chai';
+const { describe, it } = global;
+import BitFlags from '../src/BitFlags';
+
+describe('BitFlags', () => {
+	const flagArray = ['ONE', 'TWO', 'THREE', 'FOUR'];
+	const flags = new BitFlags(...flagArray);
+
+	it('should only allow less than 32 flags', () => {
+		expect(() => {
+			new BitFlags(...'flag'.repeat(32).split(''));
+		}).to.throw(Error);
+	});
+
+	it('should only contain flag values that are a power of 2', () => {
+		for (const flag of flagArray) {
+			const value = flags[flag];
+			expect(value & (value - 1)).to.equal(0);
+		}
+	});
+});
