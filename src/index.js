633baedc372476149575b9cb2dbcae2542247521
Isak W <isak.wertwein@gmail.com>
2016-09-18 02:23:35 +0200

    BitFlags & Enums

---
+++ src/index.js
@@ -0,0 +1,6 @@
+// import BitArray from './BitArray';
+// import BitField from './BitField';
+import BitFlags from './BitFlags';
+import Enum from './Enum';
+
+export { /*BitArray, BitField,*/ BitFlags, Enum };
