cb02722f8e3ea9e06ca1f27fb98c0fbcd624713b
Isak W <isak.wertwein@gmail.com>
2017-08-04 18:43:36 +0200

    Convenient assertion util functions

---
+++ src/util.js
@@ -0,0 +1,48 @@
+/**
+ * @private
+ * @param {Boolean} expression
+ * @param {String} message Error description.
+ * @throws {Error} if expression evaluates to false.
+ */
+export function assertTrue(expression, message) {
+	if (!expression) {
+		throw Error(message);
+	}
+}
+
+/**
+ * Checks whether the input value is a number. Anything that could be parsed as a number will yield false.
+ * Example: The string '1' yields false.
+ *
+ * @private
+ * @param value
+ * @returns {Boolean}
+ */
+export function isNumber(value) {
+	return typeof value === 'number';
+}
+
+/**
+ * Checks whether the input value is a integer. Anything that could be parsed as a number will yield false.
+ * Example: The string '1' yields false. The number '1.0' yields true. The number '1.1' yields false.
+ *
+ * @private
+ * @param value
+ * @returns {Boolean}
+ */
+export function isInteger(value) {
+	return isNumber(value) && Math.floor(value) === value;
+}
+
+/**
+ * Checks whether the input number is within a specific range.
+ *
+ * @private
+ * @param {Number} number
+ * @param {Number} from The inclusive lower bounds of the range.
+ * @param {Number} to The inclusive upper bounds of the range.
+ * @returns {Boolean}
+ */
+export function withinRange(number, from, to) {
+	return number >= from && number <= to;
+}
