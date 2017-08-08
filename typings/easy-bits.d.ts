789e3972898e7dbff880648aa7276e3eb20cf23f
Isak W <isak.wertwein@gmail.com>
2017-08-08 15:36:43 +0200

    Update TypeScript typings

--- typings/easy-bits.d.ts
+++ typings/easy-bits.d.ts
@@ -1,50 +1,53 @@
-
 export type Bit = 0 | 1 | false | true;
-type BitFieldType<T> = BitBase<T> | number;
 
-class BitBase<T> {
-	static valueOf(bitField: T): number;
-	static lengthOf(bitField: T): number;
+export interface BitSet<T extends BitSet | number> {
 	readonly length: number;
 	count(): number;
-	intersects(bitField: T);
-	get(index: number): number;
-	getRange(from: number, to: number): number;
+	intersects(bitset: T): boolean;
+	get(index: number): boolean;
+	getRange(from: number, to: number): BitSet<T>;
 	test(...masks: T[]): boolean;
 	testAny(...masks: T[]): boolean;
 	testAt(value: Bit, index: number): boolean;
 	testAll(value: Bit): boolean;
 	on(...masks: T[]): this;
 	off(...masks: T[]): this;
-	toggle(...masks: T[]): this;
 	set(value: Bit, ...masks: T[]): this;
 	setAll(value: Bit): this;
 	setAt(value: Bit, index: number): this;
 	setRange(value: Bit, from: number, to: number): this;
 	flip(...masks: T[]): this;
 	flipAll(): this;
+	flipAt(index: number): this;
 	flipRange(from: number, to: number): this;
 	valueOf(): number;
 	serialize(): string;
-	equals(other: BitBase): boolean;
-	toArray(): number[];
+	toArray(): boolean[];
 	toString(): string;
 }
 
-export class BitArray<T extends number = BitFieldType<T>> extends BitBase<T> {
-	constructor(minLength: number = 0);
-	static fromArray(array: any[]): BitArray<T>;
-	static copy(bitField: T): BitArray<T>;
-	static deserialize(input: string): BitArray<T>;
-	copy(bitField: T): BitArray<T>;
-	toBitField(): BitField;
+interface BitArray<T> extends BitSet<T> {
+	copy(bitset: T): BitArray<T>;
+	equals(other: BitArray<T>): boolean;
 }
 
-export class BitField<T extends number = BitFieldType<T>> extends BitBase<T> {
-	constructor(minLength: number = 0);
-	static fromArray(array: any[]): BitField<T>;
-	static copy(bitField: T): BitField<T>;
-	static deserialize(input: string): BitField<T>;
-	copy(bitField: T): BitField<T>;
-	toBitArray(): BitArray;
+interface BitArrayConstructor {
+	new <T>(minLength: number = 1): BitArray<T>;
+	fromArray<T>(array: any[]): BitArray<T>;
+	deserialize<T>(input: string): BitArray<T>;
 }
+
+export declare const BitArray: BitArrayConstructor;
+
+interface BitField<T> extends BitSet<T> {
+	copy(bitset: T): BitField<T>;
+	equals(other: BitField<T>): boolean;
+}
+
+interface BitFieldConstructor {
+	new <T>(minLength: number = 1): BitField<T>;
+	fromArray<T>(array: any[]): BitField<T>;
+	deserialize<T>(input: string): BitField<T>;
+}
+
+export declare const BitField: BitFieldConstructor;
