f56abef49101c264743422e9b1645d9a6e4c8afd
Isak W <isak.wertwein@gmail.com>
2017-08-08 04:04:10 +0200

    Simpler tests

--- test/BitField.js
+++ test/BitField.js
@@ -1,16 +1,9 @@
 const { describe, it } = global;
 import { expect } from 'chai';
+
 import BitField from '../src/BitField';
 
 describe('BitField', () => {
-	const length = 10;
-	const value = 0b0110001101;
-	const filler = '0'.repeat(length - value.toString(2).length);
-	const valueString = filler + value.toString(2);
-	const valueArray = valueString.split('').map(Number);
-	const valueArrayReversed = valueArray.slice(0).reverse();
-	const field = new BitField(length).on(value);
-
 	it('should throw an error if working with >=32 bit values', () => {
 		expect(() => {
 			new BitField(32);
@@ -19,90 +12,103 @@ describe('BitField', () => {
 
 	describe('.fromArray()', () => {
 		it('should be initialize from an array, no matter what objects are in it', () => {
-			const fieldFromArray = BitField.fromArray([1, 0, [], {}, 'a', '', undefined, null, true, false]);
-			expect(fieldFromArray.value).to.equal(0b1011100010);
+			const array = [1, 0, [], {}, 'a', '', undefined, null, true, false];
+			const bitField = BitField.fromArray(array);
+
+			expect(bitField.toArray()).to.deep.equal(array.map(Boolean));
 		});
 	});
 
 	describe('#copy()', () => {
 		it('should return a copy (clone) of the provided BitField', () => {
-			const copy1 = new BitField().copy(field);
+			const bitField = new BitField(10).copy(0b0110001101);
+			const copy = new BitField().copy(bitField);
 
-			expect(copy1.valueOf()).to.equal(field.valueOf());
-			expect(copy1).to.be.an.instanceof(BitField);
-			expect(field).not.to.equal(copy1);
+			expect(copy.valueOf()).to.equal(bitField.valueOf());
+			expect(copy).to.be.an.instanceof(BitField);
+			expect(bitField).not.to.equal(copy);
 		});
 	});
 
 	describe('#clone()', () => {
 		it('should return a copy (clone) of this', () => {
-			const copy = field.clone();
+			const bitField = new BitField(10).copy(0b0110001101);
+			const copy = bitField.clone();
 
-			expect(copy.valueOf()).to.equal(field.valueOf());
+			expect(copy.valueOf()).to.equal(bitField.valueOf());
 			expect(copy).to.be.an.instanceof(BitField);
-			expect(field).not.to.equal(copy);
+			expect(bitField).not.to.equal(copy);
 		});
 	});
 
 	describe('#toString()', () => {
 		it('should return a string', () => {
-			expect(field.toString()).to.be.a('string');
+			const bitField = new BitField(10).copy(0b0110001101);
+
+			expect(bitField.toString()).to.be.a('string');
 		});
 	});
 
 	describe('#toArray()', () => {
 		it('should return an array of booleans', () => {
-			expect(field.toArray()).to.deep.equal(valueArray.map(Boolean));
+			const bitField = new BitField(5).copy(0b1001);
+			const asArray = [false, true, false, false, true];
+
+			expect(bitField.toArray()).to.deep.equal(asArray);
 		});
 	});
 
 	describe('#equals()', () => {
 		it('should compare number/BitFields alike', () => {
-			const identicalField = new BitField(length).on(value);
+			const bitField = new BitField(10).copy(0b0110001101);
+			const copy = new BitField(10).copy(0b0110001101);
 
-			expect(field.equals(0)).to.equal(false);
-			expect(field.equals(value)).to.equal(true);
-			expect(field.equals(identicalField)).to.equal(true);
-			expect(field.equals(new BitField())).to.equal(false);
+			expect(bitField.equals(0)).to.equal(false);
+			expect(bitField.equals(0b0110001101)).to.equal(true);
+			expect(bitField.equals(copy)).to.equal(true);
+			expect(bitField.equals(new BitField())).to.equal(false);
 		});
 	});
 
 	describe('#length', () => {
 		it('should return the number the max bit amount if set, or the current used bits if not set', () => {
-			const dynamicField = new BitField().on(0b01100);
+			const bitField = new BitField(10).copy(0b0110001101);
 
-			expect(field.length).to.be.a('number');
-			expect(field.length).to.equal(length);
-			expect(BitField.lengthOf(field)).to.equal(length);
+			expect(bitField.length).to.be.a('number');
+			expect(bitField.length).to.equal(10);
 			expect(new BitField().length).to.equal(1);
-			expect(dynamicField.length).to.equal(4);
+			expect(new BitField(10).length).to.equal(10);
 		});
 	});
 
 	describe('#count()', () => {
 		it('should return the number of bits set to 1', () => {
-			const ones = valueArray.filter(number => Boolean(number)).length;
+			const bitField = new BitField(10).copy(0b0110001101);
+			const ones = bitField.toArray().filter(element => element).length;
 
-			expect(field.count()).to.be.a('number');
-			expect(field.count()).to.equal(ones);
+			expect(bitField.count()).to.be.a('number');
+			expect(bitField.count()).to.equal(ones);
+			expect(new BitField(10).count()).to.equal(0);
 		});
 	});
 
 	describe('#intersects()', () => {
 		it('should return a boolean indicating whether any bit from input BitField is also set to 1 by this', () => {
-			const otherField = field.clone();
-			otherField.flipAll();
+			const bitField = new BitField(10).copy(0b0110001101);
+			const opposite = bitField.clone().flipAll();
 
-			expect(field.intersects(0)).to.be.a('boolean');
-			expect(field.intersects(otherField)).to.equal(false);
-			expect(field.intersects(0b1010)).to.equal(true);
+			expect(bitField.intersects(0)).to.be.a('boolean');
+			expect(bitField.intersects(opposite)).to.equal(false);
+			expect(bitField.intersects(0b1010)).to.equal(true);
 		});
 	});
 
 	describe('#get()', () => {
 		it('should return bit at index', () => {
-			valueArrayReversed.forEach((bit, index) => {
-				expect(field.get(index).equals(bit)).to.equal(true);
+			const bitField = new BitField(10).copy(0b0110001101);
+
+			bitField.toArray().reverse().forEach((bit, index) => {
+				expect(bitField.get(index)).to.equal(bit);
 			});
 		});
 
@@ -117,68 +123,61 @@ describe('BitField', () => {
 
 	describe('#getRange()', () => {
 		it('should return bits within a specific range', () => {
-			const val = valueArrayReversed.slice(2, 5).reverse().join('');
+			const bitField = new BitField(5).copy(0b1101);
+			const range = bitField.getRange(2, 6);
 
-			expect(field.getRange(2, 5).equals(parseInt(val, 2))).to.equal(true);
+			expect(range.length).to.equal(4);
+			expect(range.toArray()).to.deep.equal([false, false, true, true]);
 		});
 
 		it('should throw if any index is out of bounds', () => {
 			const bitField = new BitField();
 
 			expect(() => {
-				bitField.getRange(32, 32);
+				bitField.slice(32, 33);
 			}).to.throw(Error);
 
 			expect(() => {
-				bitField.getRange(0, 32);
+				bitField.slice(0, 32);
 			}).to.throw(Error);
 		});
 	});
 
 	describe('#test()', () => {
 		it('should represent the and operation to test if ALL bits of a bitmask are set to 1', () => {
-			const mask = 0b110101011;
-			const maskArray = mask.toString(2).split('').map(Number);
-			const isTrue = maskArray.reverse().reduce((prev, curr, i) => {
-				if (Boolean(curr)) {
-					return prev && Boolean(field.get(i));
-				}
+			const bitField = new BitField(10).copy(0b0110001101);
 
-				return prev;
-			}, false);
-
-			expect(field.test(mask)).to.equal(isTrue);
-			expect(field.test(value)).to.equal(true);
-			expect(field.testAny(0)).to.equal(false);
+			expect(bitField.test(0)).to.equal(true);
+			expect(bitField.test(1)).to.equal(true);
+			expect(bitField.test(0b11)).to.equal(false);
+			expect(bitField.test(0b101)).to.equal(true);
+			expect(bitField.test(0b0110001101)).to.equal(true);
 		});
 	});
 
 	describe('#testAny()', () => {
 		it('should represent the and operation to test if ANY bits of a bitmask are set to 1', () => {
-			const mask = 0b110101011;
-			const maskArray = mask.toString(2).split('').map(Number);
-			const isTrue = maskArray.reverse().reduce((prev, curr, i) => {
-				if (Boolean(curr)) {
-					return prev || Boolean(field.get(i));
-				}
+			const bitField = new BitField(10).copy(0b0110001101);
 
-				return prev;
-			}, false);
-
-			expect(field.testAny(mask)).to.equal(isTrue);
-			expect(field.testAny(value)).to.equal(true);
-			expect(field.testAny(0)).to.equal(false);
+			expect(bitField.testAny(0)).to.equal(false);
+			expect(bitField.testAny(1)).to.equal(true);
+			expect(bitField.testAny(0b11)).to.equal(true);
+			expect(bitField.testAny(0b10)).to.equal(false);
+			expect(bitField.testAny(0b0110001101)).to.equal(true);
 		});
 	});
 
 	describe('#testAt()', () => {
 		it('should represent the and operation to test if a bit at a specific index is set to a specific value', () => {
-			valueArrayReversed.forEach((bit, index) => {
-				const isTrue = Boolean(bit);
+			const bitField = new BitField(10).copy(0b0110001101);
 
-				expect(field.testAt(1, index)).to.equal(isTrue);
-				expect(field.testAt(0, index)).to.equal(!isTrue);
+			bitField.toArray().reverse().forEach((bit, index) => {
+				expect(bitField.testAt(0, index)).to.equal(bit === false);
+				expect(bitField.testAt(1, index)).to.equal(bit === true);
 			});
+
+			expect(bitField.testAt(0, bitField.length + 1)).to.equal(true);
+			expect(bitField.testAt(1, bitField.length + 1)).to.equal(false);
 		});
 
 		it('should throw if index is out of bounds', () => {
@@ -192,14 +191,16 @@ describe('BitField', () => {
 
 	describe('#testAll()', () => {
 		it('should represent the and operation to test if ALL bits of BitField is set to a specific value', () => {
-			const falseField = new BitField(3).on(0b11);
-			const trueField = new BitField().on(0b111);
+			const bitField1 = new BitField(3);
+			const bitField2 = new BitField(3).on(0b11);
+			const bitField3 = new BitField().on(0b111);
 
-			expect(falseField.testAll(1)).to.equal(false);
-			expect(trueField.testAll(1)).to.equal(true);
-			expect(new BitField().testAll(0)).to.equal(true);
-			expect(field.testAll(1)).to.equal(valueArray.every(val => val === 1));
-			expect(field.testAll(0)).to.equal(valueArray.every(val => val === 0));
+			expect(bitField1.testAll(0)).to.equal(true);
+			expect(bitField1.testAll(1)).to.equal(false);
+			expect(bitField2.testAll(0)).to.equal(false);
+			expect(bitField2.testAll(1)).to.equal(false);
+			expect(bitField3.testAll(0)).to.equal(false);
+			expect(bitField3.testAll(1)).to.equal(true);
 		});
 	});
 
@@ -207,18 +208,17 @@ describe('BitField', () => {
 		it('should set bits to 1 based on a bitmask', () => {
 			const bitField = new BitField();
 
-			bitField.on(0b0010);
-			expect(bitField.valueOf()).to.equal(0b10);
-
 			bitField.on(0b1010);
 			expect(bitField.valueOf()).to.equal(0b1010);
+
+			bitField.on(0b11);
+			expect(bitField.valueOf()).to.equal(0b1011);
 		});
 	});
 
 	describe('#off()', () => {
 		it('should set bits to 0 based on a bitmask', () => {
-			const bitField = new BitField();
-			bitField.on(0b1010);
+			const bitField = new BitField().on(0b1010);
 
 			bitField.off(0b1000);
 			expect(bitField.valueOf()).to.equal(0b10);
@@ -287,7 +287,7 @@ describe('BitField', () => {
 			const bitField = new BitField();
 
 			expect(() => {
-				bitField.setRange(32, 32);
+				bitField.setRange(32, 33);
 			}).to.throw(Error);
 
 			expect(() => {
@@ -315,12 +315,11 @@ describe('BitField', () => {
 			bitField.flipAll();
 			expect(bitField.valueOf()).to.equal(1);
 
-			bitField.copy(0b1100);
-			bitField.flipAll();
+			bitField.copy(0b1100).flipAll();
 			expect(bitField.valueOf()).to.equal(0b11);
 
-			const bitField2 = new BitField(4);
-			bitField2.flipAll();
+			const bitField2 = new BitField(4).flipAll();
+
 			expect(bitField2.valueOf()).to.equal(0b1111);
 		});
 	});
@@ -360,7 +359,7 @@ describe('BitField', () => {
 			const bitField = new BitField();
 
 			expect(() => {
-				bitField.flipRange(32, 32);
+				bitField.flipRange(32, 33);
 			}).to.throw(Error);
 
 			expect(() => {
